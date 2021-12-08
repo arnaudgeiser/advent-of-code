5c69d64784a82d322b17a15a0e2956e5880abea7
Arnaud Geiser <arnaudgeiser@gmail.com>
2021-12-08 23:40:39 +0100

    Added Day 8

---
+++ 2021/clojure/src/advent_of_code/day9.clj
@@ -0,0 +1,6 @@
+(ns advent-of-code.day8
+  (:require [advent-of-code.core :refer [puzzle]]
+            [clojure.math.combinatorics :as combo]
+            [clojure.string :as str]))
+
+(def content (puzzle 9))
