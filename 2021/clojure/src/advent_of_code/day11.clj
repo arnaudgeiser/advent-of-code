869d65f74683608ad8f8497c2e245a2a880ac8d7
Arnaud Geiser <arnaudgeiser@gmail.com>
2021-12-12 00:38:42 +0100

    Simplify day11

--- 2021/clojure/src/advent_of_code/day11.clj
+++ 2021/clojure/src/advent_of_code/day11.clj
@@ -1,7 +1,6 @@
 (ns advent-of-code.day11
   (:require [advent-of-code.core :refer [puzzle]]
-            [clojure.string :as str]
-            [clojure.set :as set]))
+            [clojure.string :as str]))
 
 (def content (puzzle 11))
 
@@ -19,78 +18,58 @@
 
 (defn neighbors [position]
   (->> deltas
-       (map (fn [neighbor]
-              (->> neighbor
-                   (map (comp (partial reduce +) vector) position))))
-       (remove (fn [[x y]] (or (neg-int? y) (neg-int? x) (>= x height) (>= y  width))))))
+       (map (fn [neighbor] (map (comp (partial reduce +) vector) position
+                                neighbor)))
+       (remove (fn [[x y]] (or (neg-int? y)
+                               (neg-int? x)
+                               (>= x height)
+                               (>= y  width))))))
 
 (defn coords [coll]
   (for [i (range (count coll))
         j (range (count (nth coll i)))]
     [i j]))
 
-(defn step1 [matrix]
+(defn increase-matrix [matrix]
   (mapv (partial mapv inc) matrix))
 
-(defn step2 [[matrix flashed :as result] position]
+(defn handle-flashes [[matrix flashed :as result] position]
   (let [value (get-in matrix position)]
     (if (contains? flashed position)
       result
       (if (> value 9)
         (let [neigh (remove (partial contains? flashed) (neighbors position))
               inc-matrix (reduce #(update-in %1 %2 inc) matrix neigh)]
-          (reduce step2 [inc-matrix (conj flashed position)] neigh)
-          )
+          (reduce handle-flashes [inc-matrix (conj flashed position)] neigh))
         result))))
 
-(def input ["11111"
-            "19991"
-            "19191"
-            "19991"
-            "11111"])
-
-(defn do-step [matrix]
-  (let [matrix' (step1 matrix)]
-    (->> matrix'
+(defn step [matrix]
+  (let [increased-matrix (increase-matrix matrix)]
+    (->> increased-matrix
          coords
-         (reduce step2 [matrix' #{}])
+         (reduce handle-flashes [increased-matrix #{}])
          first
          (mapv (partial mapv #(if (> % 9) 0 %))))))
 
 (defn solve1 [matrix]
   (loop [i 100
-         nb-flashs 0
+         flashs 0
          matrix matrix]
     (if (zero? i)
-      nb-flashs
-      (let [new-matrix (do-step matrix)]
+      flashs
+      (let [new-matrix (step matrix)]
         (recur (dec i)
-               (+ nb-flashs (count (mapcat #(filter zero? %) new-matrix)))
+               (+ flashs (count (mapcat #(filter zero? %) new-matrix)))
                new-matrix)))))
-
-(def input2
-  ["5483143223"
-   "2745854711"
-   "5264556173"
-   "6141336146"
-   "6357385478"
-   "4167524645"
-   "2176841721"
-   "6882881134"
-   "4846848554"
-   "5283751526"]
-  )
 
 (defn solve2 [matrix]
   (loop [i 0
          matrix matrix]
     (if (every? zero? (mapcat identity matrix))
       i
-      (let [new-matrix (do-step matrix)]
+      (let [new-matrix (step matrix)]
         (recur (inc i)
                new-matrix)))))
 
-
-(comment (solve1 (mapv str->ints content)))
-
+(solve1 (map str->ints content))
 (solve2 (map str->ints content))
