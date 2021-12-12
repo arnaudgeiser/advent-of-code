6bf7f8acb89ce873243d3d38c1bef3ac93204582
Arnaud Geiser <arnaudgeiser@gmail.com>
2021-12-12 21:09:24 +0100

    Simplify day 12

--- 2021/clojure/src/advent_of_code/day12.clj
+++ 2021/clojure/src/advent_of_code/day12.clj
@@ -5,78 +5,56 @@
 
 (def content (puzzle 12))
 
-(def input
-  ["start-A"
-   "start-b"
-   "A-c"
-   "A-b"
-   "b-d"
-   "A-end"
-   "b-end"])
-
-
-(defn paths [input]
+(defn split-paths [input]
   (map #(str/split % #"-") input))
 
-(defn build-graph [paths]
-  (reduce (fn [acc [s e]] (-> acc
-                              (update s (fnil conj []) e)
-                              (update e (fnil conj []) s))) {} paths))
-
-(def graph (->> content
-                paths
-                build-graph))
+(defn build-cavern [paths]
+  (reduce (fn [acc [s e]]
+            (-> acc
+                (update s (fnil conj []) e)
+                (update e (fnil conj []) s))) {} paths))
 
 (defn small-cave? [node]
   (= node (.toLowerCase node)))
 
-(defn eligible-paths [path possible-paths]
-  (let [small-caves (set (filter small-cave? path))]
-    (set/difference (set possible-paths) small-caves)))
+(defn remove-small-caves [cave possible-caves]
+  (let [small-caves (set (filter small-cave? cave))]
+    (set/difference (set possible-caves) small-caves)))
 
-(defn solver [path graph [paths visited :as state]]
-  (if (= (last path) "end")
-    state
-    (let [possible-paths (get graph (last path))
-          eligible-paths (eligible-paths path possible-paths)]
-      (if (empty? eligible-paths)
-        state
-        (reduce (fn [[paths visited :as acc] node]
-                  (solver (conj path node) graph
-                          [(conj paths (conj path node))
-                           (set (if (small-cave? node) (conj visited node) visited))]))
-                state
-                eligible-paths)))))
-
-(->> (solver ["start"] graph [#{} #{["start"]}])
-     first
-     (filter #(= (last %) "end"))
-     count)
-
-(contains? (set/map-invert (frequencies [1 1 2 3 4 4])) 2)
-
-(defn eligible-paths2 [path possible-paths]
-  (let [small-caves (filter small-cave? path)
+(defn remove-small-caves-twice [cave possible-caves]
+  (let [small-caves (filter small-cave? cave)
         two-small-caves (contains? (set/map-invert (frequencies small-caves)) 2)]
-    (if two-small-caves
-      (set/difference (set possible-paths) (set (conj small-caves "start")))
-      (disj (set possible-paths) "start"))))
+    (disj
+     (if two-small-caves
+       (set/difference (set possible-caves) (set small-caves))
+       (set possible-caves))
+     "start")))
 
-(defn solver [path graph [paths visited :as state]]
+(defn solver [eligible-caves-fn path paths cavern]
   (if (= (last path) "end")
-    state
-    (let [possible-paths (get graph (last path))
-          eligible-paths (eligible-paths2 path possible-paths)]
-      (if (empty? eligible-paths)
-        state
-        (reduce (fn [[paths visited :as acc] node]
-                  (solver (conj path node) graph
-                          [(conj paths (conj path node))
-                           (set (if (small-cave? node) (conj visited node) visited))]))
-                state
-                eligible-paths)))))
+    paths
+    (let [possible-caves (get cavern (last path))
+          eligible-caves (eligible-caves-fn path possible-caves)]
+      (if (empty? eligible-caves)
+        paths
+        (reduce (fn [paths cave]
+                  (solver eligible-caves-fn
+                          (conj path cave)
+                          (conj paths (conj path cave))
+                          cavern))
+                paths
+                eligible-caves)))))
 
-(->> (solver ["start"] graph [#{} #{["start"]}])
-     first
-     (filter #(= (last %) "end"))
-     count)
+(defn solve [eligible-caves-fn]
+  (->> content
+       split-paths
+       build-cavern
+       (solver eligible-caves-fn ["start"] #{})
+       (filter #(= (last %) "end"))
+       count))
+
+(def solve1 (partial solve remove-small-caves))
+(def solve2 (partial solve remove-small-caves-twice))
+
+(solve1)
+(solve2)
