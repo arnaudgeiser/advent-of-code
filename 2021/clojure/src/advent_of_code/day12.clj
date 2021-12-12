e708efcbbec3c411e5bccda67b8778473743ead7
Arnaud Geiser <arnaudgeiser@gmail.com>
2021-12-12 20:31:59 +0100

    day12 part2

--- 2021/clojure/src/advent_of_code/day12.clj
+++ 2021/clojure/src/advent_of_code/day12.clj
@@ -52,3 +52,31 @@
      first
      (filter #(= (last %) "end"))
      count)
+
+(contains? (set/map-invert (frequencies [1 1 2 3 4 4])) 2)
+
+(defn eligible-paths2 [path possible-paths]
+  (let [small-caves (filter small-cave? path)
+        two-small-caves (contains? (set/map-invert (frequencies small-caves)) 2)]
+    (if two-small-caves
+      (set/difference (set possible-paths) (set (conj small-caves "start")))
+      (disj (set possible-paths) "start"))))
+
+(defn solver [path graph [paths visited :as state]]
+  (if (= (last path) "end")
+    state
+    (let [possible-paths (get graph (last path))
+          eligible-paths (eligible-paths2 path possible-paths)]
+      (if (empty? eligible-paths)
+        state
+        (reduce (fn [[paths visited :as acc] node]
+                  (solver (conj path node) graph
+                          [(conj paths (conj path node))
+                           (set (if (small-cave? node) (conj visited node) visited))]))
+                state
+                eligible-paths)))))
+
+(->> (solver ["start"] graph [#{} #{["start"]}])
+     first
+     (filter #(= (last %) "end"))
+     count)
