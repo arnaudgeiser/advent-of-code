9528cc53b5da275546cb12df7e1897e1915fdee4
Arnaud Geiser <arnaudgeiser@gmail.com>
2022-12-04 17:12:34 +0100

    fixup! fixup! Day 4

--- 2022/clojure/src/advent_of_code/day4.clj
+++ 2022/clojure/src/advent_of_code/day4.clj
@@ -1,6 +1,5 @@
 (ns advent-of-code.day4
   (:require [advent-of-code.core :refer [puzzle]]
-            [clojure.set :as set]
             [clojure.string :as str]))
 
 (def content (puzzle 4))
@@ -10,21 +9,20 @@
        (mapcat #(str/split % #","))
        (mapcat #(str/split % #"-"))
        (map parse-long)
-       (partition-all 2)
-       (map (fn [[x y]] (range x (inc y))))
-       (map set)
-       (partition-all 2)
+       (partition-all 4)
        (filter pred)
        (count)))
 
 (def solution1
   (solve
-   (fn [[l r]]
-     (or (empty? (set/difference l r))
-         (empty? (set/difference r l))))))
+   (fn [[x1 y1 x2 y2]]
+     (or (and (<= x2 x1 y2)
+              (<= x2 y1 y2))
+         (and (<= x1 x2 y1)
+              (<= x1 y2 y1))))))
 
 (def solution2
   (solve
-   (fn [[l r]]
-     (or (seq (set/intersection l r))
-         (seq (set/intersection r l))))))
+   (fn [[x1 y1 x2 y2]]
+     (or (<= x2 y1 y2)
+         (<= x1 y2 y1)))))
