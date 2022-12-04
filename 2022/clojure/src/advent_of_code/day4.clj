cfea8f13ed734875f0ac3382cba4262cf3e9da6e
Arnaud Geiser <arnaudgeiser@gmail.com>
2022-12-04 17:22:36 +0100

    fixup! fixup! fixup! Day 4

--- 2022/clojure/src/advent_of_code/day4.clj
+++ 2022/clojure/src/advent_of_code/day4.clj
@@ -16,13 +16,13 @@
 (def solution1
   (solve
    (fn [[x1 y1 x2 y2]]
-     (or (and (<= x2 x1 y2)
-              (<= x2 y1 y2))
-         (and (<= x1 x2 y1)
-              (<= x1 y2 y1))))))
+     (or (and (<= x1 x2 y1)
+              (<= x1 y2 y1))
+         (and (<= x2 x1 y2)
+              (<= x2 y1 y2))))))
 
 (def solution2
   (solve
    (fn [[x1 y1 x2 y2]]
-     (or (<= x2 y1 y2)
-         (<= x1 y2 y1)))))
+     (or (<= x1 y2 y1)
+         (<= x2 y1 y2)))))
