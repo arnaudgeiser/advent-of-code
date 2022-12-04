75c964a5545243b3f00dbf5fd627cf94368cb5ac
Arnaud Geiser <arnaudgeiser@gmail.com>
2022-12-04 20:17:54 +0100

    Day 2

---
+++ 2015/clojure/src/advent_of_code/day2.clj
@@ -0,0 +1,23 @@
+(ns advent-of-code.day2
+  (:require [advent-of-code.core :refer [puzzle]]
+            [clojure.string :as str]))
+
+(def content (puzzle 2))
+
+(def solution1
+  (->> content
+       (mapcat #(str/split % #"x"))
+       (map parse-long)
+       (partition-all 3)
+       (map (fn [[l w h :as s]] (+ (* 2 l w) (* 2 w h) (* 2 h l) (apply * (take 2 (sort s))))))
+       (reduce +)))
+
+(def solution2
+  (->> content
+       (mapcat #(str/split % #"x"))
+       (map parse-long)
+       (partition-all 3)
+       (map (fn [[l w h :as s]]
+             (let [[l1 l2] (sort s)]
+               (+ (+ l1 l1 l2 l2) (* l w h)))))
+       (reduce +)))
