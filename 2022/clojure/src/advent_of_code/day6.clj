a02784d9760b7f009cd3ce63a609e079b85b9a3e
Arnaud Geiser <arnaudgeiser@gmail.com>
2022-12-06 07:55:50 +0100

    fixup! fixup! Day 6

--- 2022/clojure/src/advent_of_code/day6.clj
+++ 2022/clojure/src/advent_of_code/day6.clj
@@ -5,9 +5,9 @@
 
 (defn solve [nb]
   (->> content
-        (partition-all nb 1)
-        (map (comp count set))
-        (reduce (fn [acc cnt] (if (= cnt nb) (reduced acc) (inc acc))) nb)))
+       (partition-all nb 1)
+       (map (comp count set))
+       (reduce (fn [acc cnt] (if (= cnt nb) (reduced acc) (inc acc))) nb)))
 
 (def solution1 (solve 4))
 (def solution2 (solve 14))
