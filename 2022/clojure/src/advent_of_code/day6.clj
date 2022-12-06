802a988e26eec8e3e52e333295d94588bdc6ee52
Arnaud Geiser <arnaudgeiser@gmail.com>
2022-12-06 07:53:27 +0100

    Day 6

---
+++ 2022/clojure/src/advent_of_code/day6.clj
@@ -0,0 +1,13 @@
+(ns advent-of-code.day6
+  (:require [advent-of-code.core :refer [puzzle]]))
+
+(def content (first (puzzle 6)))
+
+(defn solve [nb]
+ (->> content
+     (partition-all nb 1)
+     (map (comp count set))
+     (reduce (fn [acc cnt] (if (= cnt nb) (reduced acc) (inc acc))) nb)))
+
+(def solution1 (solve 4))
+(def solution2 (solve 14))
