803cae4820eaeba9f09cc499c73fe527d5975d38
Arnaud Geiser <arnaud.geiser@exoscale.ch>
2022-12-08 21:03:21 +0100

    Day 8

--- 2022/clojure/src/advent_of_code/day8.clj
+++ 2022/clojure/src/advent_of_code/day8.clj
@@ -13,34 +13,64 @@
                   (mapv #(vector (dec size) %) (range size)))))
 
 (defn row [x range] (mapv #(vector x %) range))
-
 (defn column [y range] (mapv #(vector % y) range))
 
-(defn tree-coords [coll coord]
-  (let [high (get-in content coord)]
+(defn taller-trees [coord trees]
+  (let [height (get-in content coord)]
     (loop [acc [coord]
-           coll' coll
-           high' high]
-      (if (seq coll')
-        (let [neigh (first coll')
+           rem trees
+           height' height]
+      (if (seq rem)
+        (let [neigh (first rem)
               neigh-height (get-in content neigh)
-              taller? (< high' neigh-height)]
+              taller? (< height' neigh-height)]
           (recur (if taller? (conj acc neigh) acc)
-                 (rest coll')
-                 (if taller? neigh-height high')))
+                 (rest rem)
+                 (if taller? neigh-height height')))
         acc))))
 
-(defn observe [acc [x y :as coord]]
-  (let [left  (reverse (row x (range 0 y)))
-        right (row x (range (inc y) size))
-        top (reverse (column y (range 0 x)))
-        bottom (column y (range (inc x) size))]
-    (->> (concat (tree-coords right coord)
-                 (tree-coords left coord)
-                 (tree-coords top coord)
-                 (tree-coords bottom coord))
-         (reduce (fn [acc coord] (assoc acc coord true)) acc))))
+(defn smaller-trees [coord trees]
+  (let [high (get-in content coord)]
+    (count
+     (loop [acc #{}
+            rem trees]
+       (if (seq rem)
+         (let [neigh (first rem)
+               neigh-height (get-in content neigh)]
+           (if (> high neigh-height)
+             (recur (conj acc neigh)
+                    (rest rem))
+             (conj acc neigh)))
+         acc)))))
 
-(->> (reduce observe {} coords)
-     (sort)
-     (count))
+(defn trees-lines [lookup-fn [x y :as coord]]
+  (let [left   (reverse (row x (range 0 y)))
+        right  (row x (range (inc y) size))
+        top    (reverse (column y (range 0 x)))
+        bottom (column y (range (inc x) size))]
+    (mapv (partial lookup-fn coord) [left right top bottom])))
+
+(defn observe [acc coord]
+  (->> (trees-lines taller-trees coord)
+       (reduce concat)
+       (reduce (fn [acc coord] (assoc acc coord true)) acc)))
+
+(def solution1
+  (->> (reduce observe {} coords)
+       (sort)
+       (count)))
+
+(defn tree-house-location [acc coord]
+  (->> (trees-lines smaller-trees coord)
+       (reduce *)
+       (assoc acc coord)))
+
+(def coords2
+  (for [x (range size)
+        y (range size)]
+    [x y]))
+
+(def solution2
+  (->> (reduce tree-house-location {} coords2)
+       (vals)
+       (reduce max)))
