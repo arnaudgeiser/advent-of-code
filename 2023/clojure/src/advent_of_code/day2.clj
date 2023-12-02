4ad3b845f59164bbd03eaa1dc5ccafb90f285039
Arnaud Geiser <arnaudgeiser@gmail.com>
2023-12-02 16:28:30 +0100

    Day 2

--- 2023/clojure/src/advent_of_code/day2.clj
+++ 2023/clojure/src/advent_of_code/day2.clj
@@ -4,11 +4,9 @@
 
 (def content (puzzle 2))
 
-(def l (first content))
-
-(def bag {:red 12
+(def bag {:red   12
           :green 13
-          :blue 14})
+          :blue  14})
 
 (defn parse-game [game]
   (-> game
@@ -23,22 +21,18 @@
                        (into {})))))))
 
 (defn possible-game? [game]
-  (->  game
-       (str/split #": ")
-       (second)
-       (str/split #";")
-       (->> (map (fn [round]
-                   (->> (str/split round #",")
-                        (map str/trim)
-                        (map (fn [l] (str/split l #" ")))
-                        (map (fn [[n color]] [(keyword color) (parse-long n)]))
-                        (into {}))))
-            (every? (fn [rounds]
-                      (every? (fn [[color n]]
-                                (<= n (get bag color))) rounds))))))
+  (every? (fn [rounds]
+            (every? (fn [[color n]]
+                      (<= n (get bag color))) rounds))
+          (parse-game game)))
 
-(->> (map-indexed (fn [i game] (if (possible-game? game)
-                                 (inc i)
-                                 0))
-                  content)
-     (reduce +))
+(defn solution1 []
+  (->> (map-indexed (fn [i game] (if (possible-game? game) (inc i) 0)) content)
+       (reduce +)))
+
+(defn solution2 []
+  (->> (map parse-game content)
+       (map (partial apply merge-with max))
+       (map vals)
+       (map (partial reduce *))
+       (reduce +)))
