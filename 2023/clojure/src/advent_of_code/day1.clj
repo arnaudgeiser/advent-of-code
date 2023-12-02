224e070e2e19534bc0af26ae80236cffee3f747a
Arnaud Geiser <arnaudgeiser@gmail.com>
2023-12-02 15:52:30 +0100

    Refactor Day 1

--- 2023/clojure/src/advent_of_code/day1.clj
+++ 2023/clojure/src/advent_of_code/day1.clj
@@ -5,31 +5,31 @@
 (def content (puzzle 1))
 
 (def mapping
-  {"one" "1"
+  {"one"   "1"
    "two"   "2"
    "three" "3"
-   "four" "4"
-   "five" "5"
-   "six" "6"
+   "four"  "4"
+   "five"  "5"
+   "six"   "6"
    "seven" "7"
    "eight" "8"
-   "nine" "9"})
+   "nine"  "9"})
 
-(defn solution1 [content]
+(defn parse-line1 [line]
+  (re-seq #"\d" line))
+
+(defn parse-line2 [line]
+  (->> (re-seq (re-pattern (str "(?=(\\d|" (str/join "|" (keys mapping)) "))")) line)
+       (map second)
+       (map (fn [s] (get mapping s s)))))
+
+(defn calibration [line]
+  (parse-long (str (first line) (last line))))
+
+(defn solve [content parse-fn]
   (->> content
-       (map (fn [line]
-              (->> (re-seq #"\d" line)
-                   (#(parse-long (str (first %) (last %)))))))
+       (map (comp calibration parse-fn))
        (reduce +)))
 
-(defn solution2 [content]
-  (->> content
-       (map (fn [line]
-              (->> (re-seq (re-pattern (str "(?=(\\d|" (str/join "|" (keys mapping)) "))")) line)
-                   (map second)
-                   (map (fn [s] (get mapping s s)))
-                   (#(parse-long (str (first %) (last %)))))))
-       (reduce +)))
-
-(solution1 content) ;; 55130
-(solution2 content) ;; 54985
+(solve content parse-line1) ;; 55130
+(solve content parse-line2) ;; 54985
