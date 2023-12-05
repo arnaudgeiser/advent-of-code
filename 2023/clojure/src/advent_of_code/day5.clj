63b06fe4e2e5f38978c216bc9bc93c10f4a53d53
Arnaud Geiser <arnaudgeiser@gmail.com>
2023-12-05 23:57:09 +0100

    Day 5 - Simplify

--- 2023/clojure/src/advent_of_code/day5.clj
+++ 2023/clojure/src/advent_of_code/day5.clj
@@ -1,44 +1,8 @@
 (ns advent-of-code.day5
   (:require [advent-of-code.core :refer [puzzle]]
-            [clojure.set :as set]
             [clojure.string :as str]))
 
 (def content (puzzle 5))
-
-#_(def content
-    ["seeds: 79 14 55 13"
-     ""
-     "seed-to-soil map:"
-     "50 98 2"
-     "52 50 48"
-     ""
-     "soil-to-fertilizer map:"
-     "0 15 37"
-     "37 52 2"
-     "39 0 15"
-     ""
-     "fertilizer-to-water map:"
-     "49 53 8"
-     "0 11 42"
-     "42 0 7"
-     "57 7 4"
-     ""
-     "water-to-light map:"
-     "88 18 7"
-     "18 25 70"
-     ""
-     "light-to-temperature map:"
-     "45 77 23"
-     "81 45 19"
-     "68 64 13"
-     ""
-     "temperature-to-humidity map:"
-     "0 69 1"
-     "1 0 69"
-     ""
-     "humidity-to-location map:"
-     "60 56 37"
-     "56 93 4"])
 
 (def chain [:soil :fertilizer :water :light :temperature :humidity :location])
 
@@ -67,8 +31,6 @@
   (->> (map #(reduce (fn [seed m] (transport seed (m maps))) % chain) seeds)
        (apply min)))
 
-(solution1)
-
 (defn map-transport [[s e] [src src-end diff]]
   (cond-> {}
     (< s src)
@@ -80,7 +42,6 @@
 
 (defn reduce-transports [{:keys [unmapped mapped]} conversion]
   (reduce (fn [acc' rr]
-            (prn "aa:" acc')
             (let [res (map-transport rr conversion)]
               (-> acc'
                   (update :mapped concat (:mapped res))
@@ -88,40 +49,18 @@
           {:mapped mapped}
           unmapped))
 
-(reduce-transports {:unmapped [[21 25] [10 14]] :mapped [[15 20]]} [20 25 0])
-
-(map-transport [10 12] [11 14 0])
-(map-transport [10 12] [0 7 0])
-
-(map-transport [10 25] [15 20 0])
-
-(map-transport [10 25] [5 35 0])
-
-(map-transport [10 25] [30 35 0])
-(map-transport [10 25] [0 9 0])
-
-(map-transport [10 12] [11 14 0])
-(map-transport [10 12] [10 11 0])
-
-(map-transport [10 12] [0 11 0])
-(map-transport [10 12] [0 10 0])
-
 (defn solution2 []
   (->> (partition-all 2 2 seeds)
        (map (fn [[seed size]] [seed (+ seed size)]))
        (map (fn [r]
               (reduce (fn [acc m]
-                        (prn "concat:" (concat (:mapped acc) (:unmapped acc)))
                         (let [res (reduce reduce-transports {:unmapped (concat (:mapped acc) (:unmapped acc))} (m maps))]
-                          (prn m ":" res)
                           res))
                       {:unmapped [r]}
                       chain)))
        (mapcat vals)
        (flatten)
-       (apply min)
-       #_#_#_(mapcat (fn [[seed size]] (range seed (+ seed size))))
-           (map #(reduce (fn [seed m] (transport seed (m maps))) % chain))
-         (apply min)))
+       (apply min)))
 
-#_(solution2)
+(solution1)  ;; 484023871
+(solution2)  ;; 46294175
