1b53ff19066221d5d20a1503954ea464ba47b5f4
Arnaud Geiser <arnaudgeiser@gmail.com>
2023-12-05 23:31:06 +0100

    Just save..

--- 2023/clojure/src/advent_of_code/day5.clj
+++ 2023/clojure/src/advent_of_code/day5.clj
@@ -49,19 +49,75 @@
        (drop 2)
        (partition-by #(= "" %))
        (remove #(= [""] %))
-       (map #(map (fn [numbers] (prn numbers) (vec (map parse-long (str/split numbers #" ")))) (rest %)))
+       (map #(map (fn [numbers] (vec (map parse-long (str/split numbers #" ")))) (rest %)))
+       (map #(map (fn [[dst src offset]] [src (+ src offset) (- dst src)]) %))
        (zipmap chain)))
 
 (defn transport [seed mapping]
-  (let [a (-> (keep (fn [[dst src offset]]
+  (let [a (-> (keep (fn [[src src-end diff]]
                       (when (and (>= seed src)
-                                 (<= seed (+ src offset)))
-                        (+ dst (- seed src)))) mapping)
+                                 (<= seed src-end))
+                        (+ seed diff))) mapping)
               (first))]
     (if (some? a)
       a
       seed)))
 
-(->> (map #(reduce (fn [seed m] (transport seed (m maps))) % chain) seeds)
-     (apply min)
-     (prn))
+(defn solution1 []
+  (->> (map #(reduce (fn [seed m] (transport seed (m maps))) % chain) seeds)
+       (apply min)))
+
+(solution1)
+
+(defn map-transport [[s e] [src src-end diff]]
+  (cond-> {}
+    (< s src)
+    (update :unmapped conj [s (dec (min (inc e) src))])
+    (> e src-end)
+    (update :unmapped conj [(max (inc src-end) s) e])
+    (< (max s src) (min e src-end))
+    (update :mapped conj [(+ (max s src) diff) (+ (min e src-end) diff)])))
+
+(defn reduce-transports [{:keys [unmapped] :as acc} conversion]
+  (reduce (fn [acc' rr]
+            (prn "aa:" acc')
+            (let [res (map-transport rr conversion)]
+              (-> acc'
+                  (update :mapped concat (:mapped res))
+                  (assoc :unmapped (:unmapped res)))))
+          acc
+          unmapped))
+
+(reduce-transports {:unmapped [[21 25] [10 14]] :mapped [[15 20]]} [20 25 0])
+
+(map-transport [10 12] [11 14 0])
+(map-transport [10 12] [0 7 0])
+
+(map-transport [10 25] [15 20 0])
+
+(map-transport [10 25] [5 35 0])
+
+(map-transport [10 25] [30 35 0])
+(map-transport [10 25] [0 9 0])
+
+(map-transport [10 12] [11 14 0])
+(map-transport [10 12] [10 11 0])
+
+(map-transport [10 12] [0 11 0])
+(map-transport [10 12] [0 10 0])
+
+(defn solution2 []
+  (->> (partition-all 2 2 seeds)
+       (butlast)
+       (map (fn [[seed size]] [seed (+ seed size)]))
+       (map (fn [r]
+              (reduce (fn [acc m]
+                        (prn "concat:" (concat (:mapped acc) (:unmapped acc)))
+                        (let [res (reduce reduce-transports {:unmapped (concat (:mapped acc) (:unmapped acc))} (m maps))]
+                          (prn m ":" res)
+                          res)) {:unmapped [r]} chain)))
+       #_#_#_(mapcat (fn [[seed size]] (range seed (+ seed size))))
+           (map #(reduce (fn [seed m] (transport seed (m maps))) % chain))
+         (apply min)))
+
+#_(solution2)
