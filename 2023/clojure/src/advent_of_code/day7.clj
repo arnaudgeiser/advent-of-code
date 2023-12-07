22a3ff7d6e25ef47a4e1b81759e16b6bfd2874ad
Arnaud Geiser <arnaud.geiser@exoscale.ch>
2023-12-08 00:15:48 +0100

    Day 7 - Part 2

--- 2023/clojure/src/advent_of_code/day7.clj
+++ 2023/clojure/src/advent_of_code/day7.clj
@@ -4,53 +4,37 @@
 
 (def content (puzzle 7))
 
-(def order "AKQJT98765432")
-
-(def content
-  ["32T3K 765"
-   "T55J5 684"
-   "KK677 28"
-   "KTJJT 220"
-   "QQQJA 483"])
+(def order "AKQT98765432J")
 
 (def line (-> (str/split (first content) #" ")))
 
-(def cards (first line))
-
-(def freq (frequencies (sort cards)))
-
-(defn value [hand]
-  (let [cards (frequencies hand)
-        v (vals cards)]
+(defn hand-value [hand]
+  (let [cards (dissoc (frequencies hand) \J)
+        jokers (count (filter (partial = \J) hand))
+        v (if (seq cards) (update (vec (sort (vals cards))) (dec (count cards)) + jokers)
+              [5])]
     (cond
-     (some (partial = 5) v)
-     60
-     (some (partial = 4) v)
-     50
-     (and (some (partial = 3) v)
-          (some (partial = 2) v))
-     40
-     (some (partial = 3) v)
-     30
-     (= 2 (count (filter (partial = 2) v)))
-     20
-     (= 1 (count (filter (partial = 2) v)))
-     18
-     :else 0)))
+      (some (partial = 5) v) 6
+      (some (partial = 4) v) 5
+      (and (some (partial = 3) v)
+           (some (partial = 2) v)) 4
+      (some (partial = 3) v) 3
+      (= 2 (count (filter (partial = 2) v))) 2
+      (= 1 (count (filter (partial = 2) v))) 1
+      :else 0)))
 
-(defn compare-hand [f s]
-  (prn "compare hand!" f s)
+(defn compare-cards [f s]
   (->> (map vector f s)
        (map (fn [[x y]] (- (str/index-of order x) (str/index-of order y))))
        (remove (partial = 0))
        (first)))
 
-(defn compare-hand2 [f s]
-  (if (zero? (- (value f) (value s)))
-    (compare-hand f s)
-    (- (value s)(value f))))
+(defn compare-hands [f s]
+  (if (zero? (- (hand-value f) (hand-value s)))
+    (compare-cards f s)
+    (- (hand-value s) (hand-value f))))
 
-(->> (sort #(compare-hand2 (first %1) (first %2)) (map #(str/split % #" ") content))
+(->> (sort #(compare-hands (first %1) (first %2)) (map #(str/split % #" ") content))
      (reverse)
      (map-indexed (fn[i x] (* (inc i) (parse-long (last x)))))
      (reduce +))
