5d7e525bd75c2dc9c706baf042335e7458d9b431
Arnaud Geiser <arnaud.geiser@exoscale.ch>
2023-12-08 00:38:48 +0100

    Day 7 - Clean up

--- 2023/clojure/src/advent_of_code/day7.clj
+++ 2023/clojure/src/advent_of_code/day7.clj
@@ -4,37 +4,46 @@
 
 (def content (puzzle 7))
 
-(def order "AKQT98765432J")
-
-(def line (-> (str/split (first content) #" ")))
-
 (defn hand-value [hand]
-  (let [cards (dissoc (frequencies hand) \J)
-        jokers (count (filter (partial = \J) hand))
-        v (if (seq cards) (update (vec (sort (vals cards))) (dec (count cards)) + jokers)
-              [5])]
-    (cond
-      (some (partial = 5) v) 6
-      (some (partial = 4) v) 5
-      (and (some (partial = 3) v)
-           (some (partial = 2) v)) 4
-      (some (partial = 3) v) 3
-      (= 2 (count (filter (partial = 2) v))) 2
-      (= 1 (count (filter (partial = 2) v))) 1
-      :else 0)))
+  (cond
+    (some (partial = 5) hand) 6
+    (some (partial = 4) hand) 5
+    (and (some (partial = 3) hand)
+         (some (partial = 2) hand)) 4
+    (some (partial = 3) hand) 3
+    (= 2 (count (filter (partial = 2) hand))) 2
+    (= 1 (count (filter (partial = 2) hand))) 1
+    :else 0))
 
-(defn compare-cards [f s]
+(defn compare-cards [f s order]
   (->> (map vector f s)
        (map (fn [[x y]] (- (str/index-of order x) (str/index-of order y))))
        (remove (partial = 0))
        (first)))
 
-(defn compare-hands [f s]
-  (if (zero? (- (hand-value f) (hand-value s)))
-    (compare-cards f s)
-    (- (hand-value s) (hand-value f))))
+(defn compare-hands [f s order hand-fn]
+  (let [f' (hand-fn f)
+        s' (hand-fn s)
+        diff (- (hand-value s') (hand-value f'))]
+   (if (zero? diff)
+     (compare-cards f s order)
+     diff)))
 
-(->> (sort #(compare-hands (first %1) (first %2)) (map #(str/split % #" ") content))
-     (reverse)
-     (map-indexed (fn[i x] (* (inc i) (parse-long (last x)))))
-     (reduce +))
+(defn hand-frequencies [hand]
+  (vals (frequencies hand)))
+
+(defn hand-frequencies2 [hand]
+  (let [cards (dissoc (frequencies hand) \J)
+        jokers (count (filter (partial = \J) hand))]
+    (if (seq cards)
+      (update (vec (sort (vals cards))) (dec (count cards)) + jokers)
+      [5])))
+
+(defn solve [order hand-fn]
+ (->> (sort #(compare-hands (first %1) (first %2) order hand-fn) (map #(str/split % #" ") content))
+      (reverse)
+      (map-indexed (fn [i x] (* (inc i) (parse-long (last x)))))
+      (reduce +)))
+
+(solve "AKQJT98765432" hand-frequencies) ;; 253954294
+(solve "AKQT98765432J" hand-frequencies2) ;; 254837398
