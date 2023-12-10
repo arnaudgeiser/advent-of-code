1e65c36be1cdb3648aa4e3fb0be4412984af71b0
Arnaud Geiser <arnaud.geiser@exoscale.ch>
2023-12-10 20:34:03 +0100

    Day 10 - Part 2

--- 2023/clojure/src/advent_of_code/day10.clj
+++ 2023/clojure/src/advent_of_code/day10.clj
@@ -1,33 +1,12 @@
 (ns advent-of-code.day10
-  (:require [advent-of-code.core :refer [puzzle]]
-            [clojure.string :as str]))
+  (:require [advent-of-code.core :refer [puzzle]]))
 
 (def content (puzzle 10))
 
-#_
-(def content
-  ["....."
-   ".S-7."
-   ".|.|."
-   ".L-J."
-   "....."])
-
-#_
-(def content
-  ["..F7."
-   ".FJ|."
-   "SJ.L7"
-   "|F--J"
-   "LJ..."])
-
-(def pipes
-  (->> (map (fn [coord] [coord (get-in content coord)])
-            (for [x (range (count content))
-                  y (range (count (first content)))]
-              [x y]))
-       (into {})))
-
-(def start (ffirst (filter (fn [[_ pipe]] (= pipe \S)) pipes)))
+(def h (count content))
+(def w (count (first content)))
+(def pipes (for [x (range h) y (range w)] [x y]))
+(def start (first (filter #(= (get-in content %) \S) pipes)))
 
 (def bend-directions
   {\S [:right :left :top :bottom]
@@ -57,28 +36,60 @@
     (when (tb-bends (get-in content [(inc x) y]))
       [(inc x) y])))
 
-(defn find-target2 [[x y :as coord] direction cost]
+(defn find-target+cost [[x y :as coord] direction cost]
   (when-let [[x' y' :as target] (find-target coord direction)]
     [target (+ cost (Math/abs (- x x')) (Math/abs (- y y')))]))
 
 (defn follow [[coord cost]]
- (->> (map #(find-target2 coord % cost) (bend-directions (get-in content coord)))
-      (remove nil?)))
-  
-(defn explore []
- (loop [computed {}
-        visited #{}
-        unvisited [[start 0]]]
-   (let [to-follow (first unvisited)
-         bends (follow to-follow)
-         news (remove (fn [[bend]] (visited bend)) bends)
-         unvisited' (sort #(compare (second %1) (second %2)) (concat (rest unvisited) news))]
-     (if (seq unvisited')
-       (recur (update computed (first to-follow) (fnil max 0) (second to-follow))
-              (conj visited (first to-follow))
-              unvisited')
-       (update computed (first to-follow) (fnil max 0) (second to-follow))))))
+  (->> (map #(find-target+cost coord % cost) (bend-directions (get-in content coord)))
+       (remove nil?)))
 
-(->> (explore)
-     (map second)
-     (apply max))
+(defn explore []
+  (loop [computed {}
+         visited #{}
+         unvisited [[start 0]]]
+    (let [to-follow (first unvisited)
+          bends (follow to-follow)
+          news (remove (fn [[bend]] (visited bend)) bends)
+          unvisited' (sort #(compare (second %2) (second %1)) (concat (rest unvisited) news))]
+      (if (seq unvisited')
+        (recur (update computed (first to-follow) (fnil max 0) (second to-follow))
+               (conj visited (first to-follow))
+               unvisited')
+        (update computed (first to-follow) (fnil max 0) (second to-follow))))))
+
+(defn solution1 []
+  (->> (explore)
+       (map second)
+       (apply max)
+       (inc)
+       (#(/ % 2))))
+
+(def explored (set (map first (explore))))
+
+(defn crossed? [[x y]]
+  (loop [x2 x
+         y2 y
+         crossed 0]
+    (if (and (< x2 h)
+             (< y2 w)
+             (not (explored [x y])))
+      (let [c1 (get-in content [x2 y2])]
+        (if (and (explored [x2 y2])
+                 (not= \L c1)
+                 (not= \7 c1))
+          (recur (inc x2)
+                 (inc y2)
+                 (inc crossed))
+          (recur (inc x2)
+                 (inc y2)
+                 crossed)))
+      (odd? crossed))))
+
+(defn solution2 []
+  (->> (reduce (fn [acc point] (conj acc (crossed? point))) [] pipes)
+       (filter identity)
+       (count)))
+
+(solution1) ;; 7097
+(solution2) ;; 355
