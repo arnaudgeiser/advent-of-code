8f93e5cc2cc28f6be6f863607d6e02c5bafbacf3
Arnaud Geiser <arnaudgeiser@gmail.com>
2023-12-16 00:47:36 +0100

    Day 14 - Part 2

--- 2023/clojure/src/advent_of_code/day13.clj
+++ 2023/clojure/src/advent_of_code/day13.clj
@@ -1,52 +1,29 @@
 (ns advent-of-code.day13
-  (:require [advent-of-code.core :refer [puzzle]]
+  (:require [advent-of-code.core :refer [raw-puzzle]]
             [clojure.string :as str]))
 
-(def content (puzzle 13))
+(def content (map str/split-lines (.split (raw-puzzle 13) "\\n\\n")))
 
-#_(def content ["#.##..##."
-                "..#.##.#."
-                "##......#"
-                "##......#"
-                "..#.##.#."
-                "..##..##."
-                "#.#.##.#."
-                ""
-                "#...##..#"
-                "#....#..#"
-                "..##..###"
-                "#####.##."
-                "#####.##."
-                "..##..###"
-                "#....#..#"])
+(defn mirror= [a b]
+  (= 1 (reduce + (mapcat (partial map #(if (= %1 %2) 0 1)) a b))))
 
-(defn parse []
-  (reduce (fn [acc v]
-            (let [cluster (last acc)]
-              (if (seq v)
-                (conj (vec (butlast acc)) (conj cluster v))
-                (conj acc []))))
-          [[]]
-          content))
+(defn split-point [mountain eq-fn]
+  (some (fn [i] (let [[top bottom] (split-at i mountain)
+                      top (reverse top)
+                      cnt (min (count top) (count bottom))
+                      top (take cnt top)
+                      bottom (take cnt bottom)]
+                  (when (and (seq top) (seq bottom) (eq-fn top bottom)) i)))
+        (range (count mountain))))
 
-(defn split-point [mountain]
-  (->> (filter (fn [i] (let [[top bottom] (split-at i mountain)
-                             top (reverse top)
-                             cnt (min (count top) (count bottom))
-                             top (take cnt top)
-                             bottom (take cnt bottom)]
-                         (and (seq top) (seq bottom) (= top bottom))))
-               (range (count mountain)))
-       (first)))
+(defn revert-mountain [mountain] (apply map str mountain))
 
-(defn revert-mountain [mountain]
-  (apply map str mountain))
-
-(defn split-mountain [mountain]
+(defn split-mountain [mountain eq-fn]
   (or
-   (split-point (revert-mountain mountain))
-   (* (or (split-point mountain) 0) 100)))
+   (split-point (revert-mountain mountain) eq-fn)
+   (* (or (split-point mountain eq-fn) 0) 100)))
 
-(->> (parse)
-     (map split-mountain)
-     (reduce +))
+(defn solve [eq-fn] (transduce (map #(split-mountain % eq-fn)) + content))
+
+(solve =) ;; 27664
+(solve mirror=) ;; 33991
