37165a1817f5df0961ba03be20b865dbd29ac927
Arnaud Geiser <arnaudgeiser@gmail.com>
2023-12-15 22:52:13 +0100

    Day 12 - Part 2

--- 2023/clojure/src/advent_of_code/day12.clj
+++ 2023/clojure/src/advent_of_code/day12.clj
@@ -4,44 +4,38 @@
 
 (def content (puzzle 12))
 
-#_
-(def content ["???.### 1,1,3"
-              ".??..??...?##. 1,1,3"
-              "?#?#?#?#?#?#?#? 1,3,1,6"
-              "????.#...#... 4,1,1"
-              "????.######..#####. 1,6,5"
-              "?###???????? 3,2,1"])
-
 (defn parse []
   (->> (map #(str/split % #" ") content)
        (mapv (fn [[line groups]] [line (mapv parse-long (str/split groups #","))]))))
 
-(defn valid? [[line expected]]
-  (let [cnt (->> (filter seq (re-seq #"\#*" line))
-                 (mapv count))]
-    (= cnt expected)))
+(defn valid-suffixes [row number]
+  (for [i (range (inc (- (count row) number)))
+        :while (every? #{\. \?} (take i row))
+        :when (every? #{\# \?} (take number (drop i row)))
+        :when (#{\. \?} (nth row (+ i number) \.))]
+    (drop (+ i number 1) row)))
 
-(defn question-marks [tuple]
-  (keep-indexed (fn [i x] (when (= x \?) i)) (first tuple)))
+(def acs
+  (memoize
+   (fn
+     [row numbers]
+     (if-let [[n & nrs] (seq numbers)]
+       (reduce + (for [s (valid-suffixes row n)] (acs s nrs)))
+       (if (every? #{\. \?} row) 1 0)))))
 
-(defn generate-combinations [length]
-  (if (= length 0)
-    [""]
-    (for [rest (generate-combinations (dec length))
-          s ["#" "."]]
-      (str s rest))))
+(defn *5 [[line numbers]]
+  [(str/join "?" (repeat 5 line))
+   (mapcat identity (repeat 5 numbers))])
 
-(defn possibilities [tuple]
- (let [question-marks (question-marks tuple)]
-   (->> (for [g (generate-combinations (count question-marks))]
-          (do
-            [(apply str
-                    (reduce (fn [s [idx match]]
-                              (assoc s match (nth g idx)))
-                            (vec (first tuple)) (map-indexed (fn [i x] [i x]) question-marks)))
-             (second tuple)]))
-        (filter valid?)
-        (count))))
+(defn solution1 []
+  (->> (map (partial apply acs) (parse))
+       (reduce +)))
 
-(->> (map possibilities (parse))
-     (reduce +))
+(defn solution2 []
+  (->> (parse)
+       (map *5)
+       (map (partial apply acs))
+       (reduce +)))
+
+(solution1) ;; 6981
+(solution2) ;; 4546215031609
