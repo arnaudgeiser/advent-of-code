5a4f9cb8d1b1939615cff4339b44fa8b539e6f6c
Arnaud Geiser <arnaudgeiser@gmail.com>
2023-12-16 11:15:02 +0100

    Day 14 - Part 2

--- 2023/clojure/src/advent_of_code/day14.clj
+++ 2023/clojure/src/advent_of_code/day14.clj
@@ -22,28 +22,65 @@
         y (range (count (first content)))]
     [x y]))
 
-(defn determine-position [map [x y]]
-  (loop [x x]
-    (let [v (get-in map [(dec x) y])]
+(defn determine-position [map [x y] [dx dy]]
+  (loop [x x
+         y y]
+    (let [v (get-in map [(+ x dx) (+ y dy)])]
       (if (or (nil? v) (#{\# \O} v))
         [x y]
-        (recur (dec x))))))
+        (recur (+ x dx)
+               (+ y dy))))))
 
-(defn move [map]
-  (reduce (fn [acc coord]
-            (let [v (get-in map coord)]
-              (if (= v \O)
-                (do
-                  (prn coord (determine-position acc coord))
-                  (-> acc
-                      (assoc-in coord \.)
-                      (assoc-in (determine-position acc coord) \O)))
-                acc))) map coords))
+(def mapping {:north [-1 0]
+              :south [1 0]
+              :east [0 1]
+              :west [0 -1]})
 
-(defn solve []
-  (move content))
+(def cycle [:north :west :south :east])
+(def cycles (mapcat identity (repeat cycle)))
 
-(let [solved (solve)
-      cnt (count solved)]
-  (->> (map #(* (- cnt %) (count (filter (fn [v] (= v \O)) (nth solved %)))) (range cnt))
-       (reduce +)))
+(last (take 100001 cycles))
+
+(def move
+  (memoize
+   (fn [map dir]
+     (reduce (fn [acc coord]
+               (let [v (get-in map coord)]
+                 (if (= v \O)
+                   (-> acc
+                       (assoc-in coord \.)
+                       (assoc-in (determine-position acc coord (mapping dir)) \O))
+                   acc)))
+             map
+             (if (#{:south :east} dir) (reverse coords) coords)))))
+
+(def cycle [:north :west :south :east])
+
+(defn do-cycle [map]
+  (reduce (fn [map' dir] (move map' dir)) map cycle))
+
+(defn learn [map]
+  (loop [curr map
+         knows-to-index {}
+         index 0]
+    (let [curr' (do-cycle curr)]
+      (if (seq knows-to-index)
+        (if-let [idx (knows-to-index curr')]
+          [(- index idx) idx]
+          (recur curr'
+                 (assoc knows-to-index curr' index)
+                 (inc index)))
+        (recur curr'
+               (assoc knows-to-index curr' index)
+               (inc index))))))
+
+#_#_#_(let [[nb-cycles after] (learn content)]
+        (+ after (rem (- 1000000000 after) nb-cycles)))
+
+    (defn solve []
+      (reduce (fn [acc v] (do-cycle acc)) content (range 109)))
+
+  (let [solved (solve)
+        cnt (count solved)]
+    (->> (map #(* (- cnt %) (count (filter (fn [v] (= v \O)) (nth solved %)))) (range cnt))
+         (reduce +)))
