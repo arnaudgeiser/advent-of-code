3c04c0b32a72e57362b4a279581f5b6b977c88fe
Arnaud Geiser <arnaudgeiser@gmail.com>
2023-12-17 19:51:32 +0100

    Day 15 - Part 2

--- 2023/clojure/src/advent_of_code/day15.clj
+++ 2023/clojure/src/advent_of_code/day15.clj
@@ -2,32 +2,31 @@
   (:require [advent-of-code.core :refer [raw-puzzle]]
             [clojure.string :as str]))
 
-(def content (str/replace (raw-puzzle 15) "\n" ""))
-
-(def content "rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7")
-
-(def lines (str/split content #","))
+(def content (str/split (str/replace (raw-puzzle 15) "\n" "") #","))
 
 (defn compute-hash [line]
-  (reduce (fn [acc c]
-            (let [i (+ acc (int c))
-                  i (* 17 i)
-                  i (mod i 256)]
-              i)) 0 line))
+  (reduce (fn [acc c] (mod (* 17 (+ acc (int c))) 256)) 0 line))
 
-(->> (map compute-hash lines)
-     (reduce +))
+(defn solution1 [] (reduce + (map compute-hash content)))
 
-(def res (reduce (fn [acc line]
-                   (let [[label sign fl] (rest (re-find #"(.*)([=-])(.*)" line))
-                         hash (compute-hash label)]
-                     (condp = sign
-                       "=" (update acc hash (fnil assoc {}) label (parse-long fl))
-                       "-" (do
-                             (update acc hash dissoc label)))))
-                 {}
-                 lines))
+(defn boxes [content]
+  (reduce (fn [acc line]
+            (let [[label sign fl] (rest (re-find #"(.*)([=-])(.*)" line))
+                  hash (compute-hash label)]
+              (condp = sign
+                "=" (update acc hash (fnil assoc {}) label (parse-long fl))
+                "-" (update acc hash dissoc label))))
+          {}
+          content))
 
-(->> (mapcat (fn [fp]
-               (map-indexed (fn [slot [_ fl]] (* (inc fp) (inc slot) fl)) (get res fp)))  (range 256))
-     (reduce +))
+(defn focusing-power [boxes box-nb]
+  (map-indexed (fn [slot [_ fl]] (* (inc box-nb) (inc slot) fl))
+               (get boxes box-nb)))
+
+(defn solution2 []
+  (let [boxes (boxes content)]
+    (->> (mapcat (partial focusing-power boxes) (range 256))
+         (reduce +))))
+
+(solution1) ;; 515495
+(solution2) ;; 229349
