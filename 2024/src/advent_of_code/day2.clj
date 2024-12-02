5e44fe2ef03c523f8a6cd0a17db6bc85c9b4fb69
Arnaud Geiser <arnaudgeiser@gmail.com>
2024-12-02 19:21:40 +0100

    Advent of Code - Day 2

---
+++ 2024/src/advent_of_code/day2.clj
@@ -0,0 +1,19 @@
+(ns advent-of-code.day2
+  (:require [advent-of-code.core :refer [puzzle submit]]
+            [clojure.string :as str]))
+
+(def content (map #(map parse-long (str/split % #" ")) (puzzle 2)))
+
+(defn safe? [line]
+  (and (or (apply < line)
+           (apply > line))
+       (every? (fn [[v1 v2]] (prn v1 v2) (<= 1 (Math/abs (- v1 v2)) 3)) (partition 2 1 line))))
+
+(defn varients [line]
+  (map #(vec (concat (take % line) (drop (inc %) line))) (range (count line))))
+
+(def solution1 (count (filter safe? content)))
+(def solution2 (count (filter #(some safe? (varients %)) content)))
+
+(prn solution1) ;; 230
+(prn solution2) ;; 301
